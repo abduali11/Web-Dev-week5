7db843a79cc92ad4989702361fb0da48cbd44866
Abduali11 <abdulali@metropolia.fi>
2024-04-24 13:43:08 +0300

    viesti

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const MediaRow = (props) => {
-  const {item} = props;
+const MediaRow = ({item}) => {
+
 
   return (
-    <tr key={item.media_id}>
+    <tr>
       <td>
         <img src={item.thumbnail} alt={item.title} />
       </td>
       <td>{item.title}</td>
+      <td>{item.username}</td>
       <td>{item.description}</td>
       <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
       <td>{item.filesize}</td>
--- src/components/Single.jsx
+++ src/components/Single.jsx
@@ -1,16 +1,28 @@
-import { useLocation, useNavigate } from "react-router-dom"
+import { useLocation, useNavigate, useParams, } from "react-router-dom"
 import SingleView from "./SingleView"
+import  mediaArray from "../data/Media.js"
+
 
 const Single = () => {
   const location = useLocation()
   const navigate = useNavigate()
+  const params = useParams()
+
+
+  const id = Number(params.id)
+let item;
+
 
   if (!location.state?.item) {
-    return null
+
+  item = mediaArray.find(({media_id}) => Number(media_id) === id)
+  }else{
+    item = location.state.item
   }
 
-  const { item } = location.state
-  console.log(item)
+  if(!item){
+    return <>Media not found</>
+  }
   return (
     <SingleView
     selectedItem={item}
