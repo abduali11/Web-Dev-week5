499437688c26c006c5bbacaef6f5b281a48aa951
Abduali11 <abdulali@metropolia.fi>
2024-04-24 14:19:37 +0300

    viesti

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~abdulali/hooks/',
+  base: '/~abdulali/custom-hooks1/',
 });
